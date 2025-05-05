339fb25f4d599e735885d9427509d1e4c98e8fbd
SERGIO TEJEDOR MORENO <119494713+sertemo@users.noreply.github.com>
2025-05-05 10:35:45 +0100

    Actualizar up.sh

--- up.sh
+++ up.sh
@@ -2,5 +2,5 @@
 
 docker rm shitplit && docker rmi shitplit
 docker build -t shitplit . && \
-docker run -e DB_MONGO='mongodb+srv://sergioDBPython:pythonDBsergio1234%40@pythondbtest.q8zkki8.mongodb.net/?retryWrites=true&w=majority' \
+docker run -e DB_MONGO='retryWrites=true&w=majority' \
     -p 60751:60751 --name shitplit shitplit:latest
